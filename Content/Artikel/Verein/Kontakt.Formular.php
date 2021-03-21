8581353e15e3de3515b0a44567d18e95c90248b1
juergenschuft <juergen.schuft@web.de>
2021-03-21 13:48:35 +0100

    add contact form

---
+++ Content/Artikel/Verein/Kontakt.Formular.php
@@ -0,0 +1,41 @@
+<table border="0">
+<tr>
+<td colspan="5">
+<div>
+		&nbsp;
+		</div>
+<div>
+		<strong>Ihre Anfrage an uns:</strong><br />&nbsp;
+		</div>
+</td>
+</tr>
+<tr>
+<td>Vorname:</td>
+<td>###name:Vorname; type:text; valid:name; mandatory:true; class:kontakt; ### </td>
+<td>Telefon:</td>
+<td>###name:Telefon; type:text; ### </td>
+</tr>
+<tr>
+<td>Nachname:</td>
+<td>###name:Nachname; type:text; valid:text;  ###</td>
+<td>Email:</td>
+<td>###name:Email; type:text; valid:email; mandatory:true;### </td>
+</tr>
+<tr>
+<td valign="top">&nbsp;<br />Nachricht:</td>
+<td colspan="3">&nbsp;<br />###name:Nachricht; type:textarea; cols:45; rows:8 ### </td>
+</tr>
+<tr>
+<td colspan="5" align="right" valign="top"><input type="submit" name="Submit" value="Abschicken" /></td>
+</tr>
+</table>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
