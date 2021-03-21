13a5d352db4dcd73b68e2ceb51855b14ea96310c
juergenschuft <juergen.schuft@web.de>
2021-03-21 13:57:46 +0100

    add Tandempringen_Gutscheinbestellung

---
+++ Content/Artikel/Tandemspringen/BestellungFrage/Gutscheinbestellung.Formular.php
@@ -0,0 +1,65 @@
+<p>&nbsp;</p>
+<table border="0">
+<tr>
+<td colspan="5">pers&ouml;nliche Angaben:</td>
+</tr>
+<tr>
+<td>Vorname:</td>
+<td>###name:Vorname; type:text; valid:name; mandatory:true; class:kontakt; ###</td>
+<td>Telefon:</td>
+<td>###name:Telefon; type:text; ###</td>
+</tr>
+<tr>
+<td>Nachname:</td>
+<td>###name:Nachname; type:text; valid:text; ###</td>
+<td>Email:</td>
+<td>###name:Email; type:text; valid:email; mandatory:true;###</td>
+</tr>
+</table>
+<p>&nbsp;</p>
+<table border="0">
+<tr>
+<td>###name:Frage; type:checkbox; valid:text; class:kontakt; ###</td>
+<td colspan="3">Ich habe nur eine Frage</td>
+</tr>
+</table>
+<table border="0">
+<tr>
+<td colspan="5">Ich m&ouml;chte folgendes bestellen:</td>
+</tr>
+<tr>
+<td>###name:Tandemsprung; type:checkbox; valid:text; class:kontakt; ###</td>
+<td colspan="3">Tandemsprung (195,- &euro;):</td>
+</tr>
+<tr>
+<td>###name:Tandemsprung_mit_Video; type:checkbox; valid:text; ###</td>
+<td colspan="3">Tandemsprung mit Video (285,- &euro;):</td>
+</tr>
+<tr>
+<td>###name:Gutschein; type:checkbox; valid:text; class:kontakt; ###</td>
+<td colspan="3">Geschenkgutschein f&uuml;r einen Tandemsprung (200,- &euro;)</td>
+</tr>
+<tr>
+<td>###name:Gutschein_mit_Video; type:checkbox; valid:text; ###</td>
+<td colspan="3">Geschenkgutschein f&uuml;r einen Tandemsprung mit Video (290,- &euro;):</td>
+</tr>
+</table>
+<p>&nbsp;</p>
+<table border="0">
+<tr>
+<td colspan="3">Bemerkungen/W&uuml;nsche:</td>
+</tr>
+<tr>
+<td colspan="3">###name:Nachricht; type:textarea; cols:35; rows:5 mandatory:true###</td>
+</tr>
+<tr>
+<td colspan="5" align="right" valign="top"><input type="submit" name="Submit" value="Abschicken" /></td>
+</tr>
+</table>
+<p>&nbsp;</p>
+<p>&nbsp;Ihr k&ouml;nnt uns nat&uuml;rlich auch gern unter&nbsp;+49 174 4715111 anrufen.</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
