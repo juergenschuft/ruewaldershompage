ad9eae2ca4197dfbaffcbee8b1941609f979690f
juergenschuft <juergen.schuft@web.de>
2021-03-21 11:44:17 +0100

    add AnmeldungZurAusbildung

---
+++ Content/Artikel/Ausbildung/Anmeldung_Fragen/AnmeldungZurAusbildung.Formular.php
@@ -0,0 +1,61 @@
+<p>&nbsp;</p>
+<table border="0">
+<tr>
+<td colspan="5">pers&ouml;nliche Angaben:</td>
+</tr>
+<tr>
+<td>Vorname:</td>
+<td>###name:Vorname; type:text; valid:name; mandatory:true; class:kontakt; ###</td>
+<td>Telefon:</td>
+<td>###name:Telefon; type:text; ###</td>
+</tr>
+<tr>
+<td>Nachname:</td>
+<td>###name:Nachname; type:text; valid:text; ###</td>
+<td>Email:</td>
+<td>###name:Email; type:text; valid:email; mandatory:true;###</td>
+</tr>
+</table>
+<p>&nbsp;</p>
+<table border="0">
+<tr>
+<td>###name:Frage; type:checkbox; valid:text; class:kontakt; ###</td>
+<td colspan="3">Ich habe nur eine Frage</td>
+</tr>
+</table>
+<table border="0">
+<tr>
+<td colspan="5">Ich m&ouml;chte mich anmelden f&uuml;r:</td>
+</tr>
+<tr>
+<td>###name:Grundkursanmeldung; type:checkbox; valid:text; class:kontakt; ###</td>
+<td colspan="3">Grundkurs f&uuml;r 399,- Euro</td>
+</tr>
+<tr>
+<td>###name:Freifallkursanmeldung; type:checkbox; valid:text; ###</td>
+<td colspan="3">Freifallkurs f&uuml;r 480,- Euro</td>
+</tr>
+<tr>
+<td>###name:PPL-Kursanmeldung; type:checkbox; valid:text; class:kontakt; ###</td>
+<td colspan="3">PPL-F Erlaubniskurs f&uuml;r 225,- Euro</td>
+</tr>
+</table>
+<p>&nbsp;</p>
+<table border="0">
+<tr>
+<td colspan="3">Bemerkungen/ W&uuml;nsche/ Fragen:</td>
+</tr>
+<tr>
+<td colspan="3">###name:Nachricht; type:textarea; cols:35; rows:5 ###</td>
+</tr>
+<tr>
+<td colspan="5" align="right" valign="top"><input type="submit" name="Submit" value="Abschicken" /></td>
+</tr>
+</table>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
